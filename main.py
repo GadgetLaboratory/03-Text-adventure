4a2cf00b6a8141510f111bb5839e967d6972323d
GadgetLaboratory <crazygadget466@gmail.com>
2019-09-17 13:42:53 -0400

    Assembled resources; created outlines for classes

---
+++ main.py
@@ -0,0 +1,12 @@
+class Character: #define stats here: name, about, health, energy, strength, defense, agility
+    def __init__(self, name, about, hp, mp, str, vit, agi):
+
+class Skill: #define flavor, power, effects, type, element
+    def __init__(self, flavor, power, effect, type, element):
+
+class Item: #define flavor, effects, uses
+    def __init__(self, flavor, effect, uses):
+
+class Equipment: #define flavor, effects, stats changes
+    def __init__(self, flavor, effect, stat):
+
